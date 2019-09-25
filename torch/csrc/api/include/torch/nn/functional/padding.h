8b5d3929556cebcc4ea871d4ba3e52eb7eaa1b51
joshvarty@gmail.com <joshvarty@gmail.com>
2019-09-24 23:52:24 -0400

    Rework call to reflection_pad1d

--- torch/csrc/api/include/torch/nn/functional/padding.h
+++ torch/csrc/api/include/torch/nn/functional/padding.h
@@ -1,19 +1,27 @@
 #pragma once
 
 #include <torch/nn/options/padding.h>
+#include <torch/expanding_array.h>
+
 
 namespace torch {
 namespace nn {
 namespace functional {
 
 inline Tensor reflection_pad1d(const Tensor& input, const ReflectionPad1dOptions& options) {
+
+    //TODO: There must be a cleaner way to do this?
+    auto paddingArray = options.padding();
+    auto paddingValue = paddingArray->front();
+    auto paddingAsInt = (int)paddingValue;
+
     return torch::reflection_pad1d(
         input,
-        options.padding());
+        {paddingAsInt, paddingAsInt});
 }
 
 inline Tensor reflection_pad2d(const Tensor& input, const ReflectionPad2dOptions& options) {
-    return torch::reflection_pad1d(
+    return torch::reflection_pad2d(
         input,
         options.padding());
 }
